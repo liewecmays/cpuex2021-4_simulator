d3be6c3963241f0c473392ab294737af64a43e9a
liewecmays <liewecmays.0802@gmail.com>
2021-11-19 00:58:05 +0900

    update[sim]: add some operations to ./fpu_test

--- simulator/fpu_test.cpp
+++ simulator/fpu_test.cpp
@@ -11,8 +11,10 @@ using ull = unsigned long long;
 
 // 定数の設定
 Bit32 e127_32 = {0x7f000000};
+Bit32 e31_32 = {0x4f000000};
 Bit64 e_23 = {0x3e80000000000000};
 Bit64 e_22 = {0x3e90000000000000};
+Bit64 e_20 = {0x3eb0000000000000};
 Bit64 e_126 = {0x3810000000000000};
 Bit64 e127 = {0x47e0000000000000};
 
@@ -25,25 +27,32 @@ int main(){
     std::mt19937 mt(rnd());
 
     Bit32 x1, x2, y;
-    Ftype t = Ftype::o_fsub;
+    Ftype t = Ftype::o_floor;
     for(int i=0; i<10; i++){     
         x1.ui = mt();
         if(is_invalid(x1)) break;
-        if(is_single_arg(t)){
-            x2 = Bit32(0);
-        }else{
+        if(has_two_args(t)){
             x2.ui = mt();
             if(is_invalid(x2)) break;
+        }else{
+            x2 = Bit32(0);
         }
 
         y = calc_fpu(x1, x2, t);
 
         if(is_invalid(y)){
             std::cout << "\x1b[1m" << string_of_ftype(t) << ": \x1b[31minvalid value\x1b[0m" << std::endl;
-        }else if(verify(x1, x2, y, t)){
-            std::cout << "\x1b[1m" << string_of_ftype(t) << ": \x1b[31mtoo large error\x1b[0m" << std::endl;
             std::cout << std::setprecision(10) << "  x1 = " << x1.f << "\t(" << x1.to_string(Stype::t_bin) << ")" << std::endl;
-            std::cout << std::setprecision(10) << "  x2 = " << x2.f << "\t(" << x2.to_string(Stype::t_bin) << ")" << std::endl;
+            if(has_two_args(t)){
+                std::cout << std::setprecision(10) << "  x2 = " << x2.f << "\t(" << x2.to_string(Stype::t_bin) << ")" << std::endl;
+            }
+            std::cout << std::setprecision(10) << "  y  = " << y.f << "\t(" << y.to_string(Stype::t_bin) << ")" << std::endl;
+        }else if(verify(x1, x2, y, t)){
+            std::cout << "\x1b[1m" << string_of_ftype(t) << ": \x1b[31mdoes not meet specification\x1b[0m" << std::endl;
+            std::cout << std::setprecision(10) << "  x1 = " << x1.f << "\t(" << x1.to_string(Stype::t_bin) << ")" << std::endl;
+            if(has_two_args(t)){
+                std::cout << std::setprecision(10) << "  x2 = " << x2.f << "\t(" << x2.to_string(Stype::t_bin) << ")" << std::endl;
+            }
             std::cout << std::setprecision(10) << "  y  = " << y.f << "\t(" << y.to_string(Stype::t_bin) << ")" << std::endl;
         }
     }
@@ -74,6 +83,28 @@ bool verify(Bit32 x1, Bit32 x2, Bit32 y, Ftype t){
                 && -e127_32.f < x2.f && x2.f < e127_32.f
                 && -e127.d < d_std && d_std < e127.d
                 && std::abs(d_y - d_std) >= std::max(std::abs(d_std) * e_22.d, e_126.d);
+        case Ftype::o_fdiv:
+            return
+                -e127_32.f < x1.f && x1.f < e127_32.f
+                && -e127_32.f < x2.f && x2.f < e127_32.f
+                && -e127.d < d_std && d_std < e127.d
+                && x2.f != 0.0f
+                && std::abs(d_y - d_std) >= std::max(std::abs(d_std) * e_20.d, e_126.d);
+        case Ftype::o_fsqrt:
+            return
+                0.0f <= x1.f && x1.f < e127_32.f
+                && std::abs(d_y - d_std) >= std::max(d_std * e_20.d, e_126.d);
+        // case Ftype::o_itof:
+        //     return ???
+        // case Ftype::o_ftoi:
+        //     return ???
+        //         -e31_32.f + 1 <= x1.f && x1.f <= e31_32.f - 1
+        //         &&
+        case Ftype::o_floor:
+            return
+                -e127_32.f < x1.f && x1.f < e127_32.f
+                && (/*std::nearbyint(d_y) != d_y // 整数ではない
+                ||*/ d_y > d_x1 || d_x1 >= d_y);
         default:
             std::cerr << "internal error" << std::endl;
             std::exit(EXIT_FAILURE);
@@ -87,6 +118,18 @@ Bit32 calc_fpu(Bit32 x1, Bit32 x2, Ftype t){
             return fadd(x1, x2);
         case Ftype::o_fsub:
             return fsub(x1, x2);
+        case Ftype::o_fmul:
+            return fmul(x1, x2);
+        case Ftype::o_fdiv:
+            return fdiv(x1, x2);
+        case Ftype::o_fsqrt:
+            return fsqrt(x1);
+        case Ftype::o_itof:
+            return itof(x1);
+        case Ftype::o_ftoi:
+            return ftoi(x1);
+        case Ftype::o_floor:
+            return floor(x1);
         default:
             std::cerr << "internal error" << std::endl;
             std::exit(EXIT_FAILURE);
@@ -104,6 +147,16 @@ double calc_std(Bit32 x1, Bit32 x2, Ftype t){
             return d_x1 - d_x2;
         case Ftype::o_fmul:
             return d_x1 * d_x2;
+        case Ftype::o_fdiv:
+            return d_x1 / d_x2;
+        case Ftype::o_fsqrt:
+            return std::sqrt(d_x1);
+        case Ftype::o_itof:
+            return static_cast<double>(x1.i);
+        case Ftype::o_ftoi:
+            return std::nearbyint(d_x1);
+        case Ftype::o_floor:
+            return 0.0; // 比較対象がいらない
         default:
             std::cerr << "internal error" << std::endl;
             std::exit(EXIT_FAILURE);
@@ -117,9 +170,9 @@ inline bool is_invalid(Bit32 b){
         || (b.F.e == 255); // infを除く
 }
 
-// 引数が1つかどうかを判定
-inline bool is_single_arg(Ftype t){
-    if(t == Ftype::o_finv || t == Ftype::o_fsqrt || t == Ftype::o_ftoi || t == Ftype::o_itof || t == Ftype::o_floor){
+// 引数が2つかどうかを判定
+inline bool has_two_args(Ftype t){
+    if(t == Ftype::o_fadd || t == Ftype::o_fsub || t == Ftype::o_fmul || t == Ftype::o_fdiv){
         return true;
     }else{
         return false;
--- simulator/fpu_test.hpp
+++ simulator/fpu_test.hpp
@@ -16,6 +16,6 @@ bool verify(Bit32, Bit32, Bit32, Ftype); // 実装基準を満たしているか
 double calc_std(Bit32, Bit32, Ftype); // 比較対象の値を返す
 Bit32 calc_fpu(Bit32, Bit32, Ftype); // FPUによる計算(1引数の場合第2引数は無視)
 bool is_invalid(Bit32); // 有効でない浮動小数点数を判定
-bool is_single_arg(Ftype); // 引数が1つかどうかを判定
+bool has_two_args(Ftype); // 引数が2つかどうかを判定
 std::string string_of_ftype(Ftype); // Ftypeを文字列に変換
 double max_of_4(double, double, double, double);
