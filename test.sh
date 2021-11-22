d41ea2540ceac0cc57338e9d4a5d972f572a2c3d
daiki0802 <daiki-matsuoka@g.ecc.u-tokyo.ac.jp>
2021-11-23 07:31:08 +0900

    fix: test.sh option

--- test.sh
+++ test.sh
@@ -9,7 +9,7 @@ MEMORY=""
 IS_SKIP=""
 IS_RAYTRACING=""
 IS_BIN=""
-while getopts f:odp:bm:sr-: OPT
+while getopts f:idp:bm:sr-: OPT
 do
     case $OPT in
         -)
@@ -17,7 +17,7 @@ do
                 boot) IS_BOOTLOADING="--boot";;
             esac;;
         f) FILENAME=$OPTARG;;
-        o) IS_INFO_OUT="-o";;
+        i) IS_INFO_OUT="-i";;
         d) IS_DEBUG="-d";;
         p) PORT="-p ${OPTARG}";;
         b) IS_BIN="-b";;
