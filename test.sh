b73a949d03763190f452f737c3cad0ca83a94717
liewecmays <liewecmays.0802@gmail.com>
2022-02-13 17:43:29 +0900

    change: integrate test2.sh into test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+IS_SECOND=""
 FILENAME=""
 IS_INFO_OUT=""
 IS_DEBUG=""
@@ -12,7 +13,7 @@ IS_RAYTRACING=""
 IS_BIN=""
 IS_PRELOADING=""
 IS_IEEE=""
-while getopts f:bdim:srp:b-: OPT
+while getopts 2f:bdim:srp:b-: OPT
 do
     case $OPT in
         -)
@@ -22,6 +23,7 @@ do
                 boot) IS_BOOTLOADING="--boot";;
                 stat) IS_STAT="--stat";;
             esac;;
+        2) IS_SECOND="2nd";;
         f) FILENAME=$OPTARG;;
         d) IS_DEBUG="-d";;
         b) IS_BIN="-b";;
@@ -49,8 +51,13 @@ cd ../ || exit 1
 cp assembler/out/$FILENAME$EXT simulator/code/$FILENAME$EXT || exit 1
 cd simulator || exit 1
 
-if [ "$PORT" != "" -o "$IS_BOOTLOADING" != "" -o "$IS_STAT" != "" ]; then
-    rlwrap ./sim-d -f $FILENAME $IS_BIN $IS_DEBUG $IS_INFO_OUT $MEMORY $IS_IEEE $IS_SKIP $IS_PRELOADING $IS_RAYTRACING $PORT $IS_BOOTLOADING $IS_STAT || exit 1
+
+if [ "${IS_SECOND}" != "" ]; then
+    rlwrap ./sim2 -f $FILENAME $IS_BIN $IS_DEBUG $IS_INFO_OUT $MEMORY $IS_IEEE $IS_SKIP $IS_PRELOADING $IS_RAYTRACING $PORT || exit 1
 else
-    rlwrap ./sim -f $FILENAME $IS_BIN $IS_DEBUG $IS_INFO_OUT $MEMORY $IS_IEEE $IS_SKIP $IS_PRELOADING $IS_RAYTRACING || exit 1
+    if [ "$PORT" != "" -o "$IS_BOOTLOADING" != "" -o "$IS_STAT" != "" ]; then
+        rlwrap ./sim-d -f $FILENAME $IS_BIN $IS_DEBUG $IS_INFO_OUT $MEMORY $IS_IEEE $IS_SKIP $IS_PRELOADING $IS_RAYTRACING $PORT $IS_BOOTLOADING $IS_STAT || exit 1
+    else
+        rlwrap ./sim -f $FILENAME $IS_BIN $IS_DEBUG $IS_INFO_OUT $MEMORY $IS_IEEE $IS_SKIP $IS_PRELOADING $IS_RAYTRACING || exit 1
+    fi
 fi
