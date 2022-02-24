9eeb11f8d3df6be96bbe4011de315c1f510a49ba
liewecmays <liewecmays.0802@gmail.com>
2022-02-24 15:32:40 +0900

    fix: remove an unnecessary option from server.sh

--- server.sh
+++ server.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-IS_DEBUG=""
+# IS_DEBUG=""
 PORT=""
-while getopts dp: OPT
+while getopts p: OPT
 do
     case $OPT in
-        d) IS_DEBUG="-d";;
         p) PORT="-p ${OPTARG}";;
     esac
 done
 
 cd simulator || exit 1
-rlwrap ./server $IS_DEBUG $PORT || exit 1
+rlwrap ./server $PORT || exit 1
